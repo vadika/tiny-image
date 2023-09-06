0a78c2669bd13ec4771d4a39e4515c92a1016889
vadik likholetov <vadikas@gmail.com>
2023-09-06 12:02:20 +0300

    -amend

---
+++ run-tiny-image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+qemu-system-x86_64 \
+    -kernel /boot/vmlinuz-$(uname -r) \
+    -initrd tiny-initrd.img \
+    -append 'console=ttyS0 quiet' \
+    -m 1000 -display none -serial stdio -accel kvm 
